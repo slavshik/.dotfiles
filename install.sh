2c88208fcce910c6483e9c2a870e5cdabe0a6e1e
Alexander <slavshik@me.com>
2022-09-21 19:35:19 +0200

    fix: alacritty config added

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ DOTFILES=~/.dotfiles
 ln -s $DOTFILES/zsh/.zshrc ~/.zshrc
 ln -s $DOTFILES/nvim/.config/nvim ~/.config
 ln -s $DOTFILES/tmux/.tmux.conf ~/.tmux.conf
+ln -s $DOTFILES/alacritty ~/.config
