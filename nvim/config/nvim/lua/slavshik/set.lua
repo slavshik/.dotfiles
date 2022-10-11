4345a515c709fe22fbc41ccf889172408f228745
Alexander <slavshik@me.com>
2022-10-11 16:54:36 +0200

    fix (LazyGit): improvements

--- nvim/config/nvim/lua/slavshik/set.lua
+++ nvim/config/nvim/lua/slavshik/set.lua
@@ -34,3 +34,11 @@ vim.opt.updatetime = 50
 vim.opt.signcolumn = "yes" -- git/errors/linters SHOW always to prevent blinking and shifting
 --vim.opt.hi="Normal guibg=NONE ctermbg=NONE"
 --vim.opt.hi = "Normal"
+--
+-- LazyGit
+--
+vim.g.lazygit_floating_window_winblend = 1.0 -- transparency of floating window
+vim.g.lazygit_floating_window_scaling_factor = 1.0 -- scaling factor for floating window
+-- vim.g.lazygit_floating_window_corner_chars = ['╭', '╮', '╰', '╯'] -- customize lazygit popup window corner characters
+-- vim.g.lazygit_floating_window_use_plenary = 1 -- use plenary.nvim to manage floating window if available
+-- vim.g.lazygit_use_neovim_remote = 1 -- fallback to 0 if neovim-remote is not installed
