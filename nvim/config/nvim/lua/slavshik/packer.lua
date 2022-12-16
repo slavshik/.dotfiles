eb06ec0a16b0ebef96368a3ca564c6cd0c30f5d4
Aliaksandr Slaushchyk <slavsik@Aliaksandrs-MacBook-Pro.local>
2022-12-16 22:16:43 +0100

    fix: added treesitter

--- nvim/config/nvim/lua/slavshik/packer.lua
+++ nvim/config/nvim/lua/slavshik/packer.lua
@@ -34,6 +34,7 @@ return require("packer").startup(function(use)
 	use("BurntSushi/ripgrep")
 	use("nvim-telescope/telescope-fzy-native.nvim")
 	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+	use("nvim-treesitter/playground")
 	use("sumneko/lua-language-server")
 	-- Lua
 	use({
