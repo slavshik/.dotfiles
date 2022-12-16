80fe61275bcf4a4aa7f6fa6bcb17f6e18c59016b
Aliaksandr Slaushchyk <slavsik@Aliaksandrs-MacBook-Pro.local>
2022-12-16 22:22:13 +0100

    fix (karabiner): added config

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ DOTFILES=~/.dotfiles
 ln -s $DOTFILES/zsh/zshrc ~/.zshrc
 ln -s $DOTFILES/nvim/config/nvim ~/.config/nvim
 ln -s $DOTFILES/tmux/tmux.conf ~/.tmux.conf
-ln -s $DOTFILES/alacritty ~/.config
+ln -s $DOTFILES/alacritty ~/.config/alacritty
+ln -s $DOTFILES/karabiner ~/.config/karabiner
 ln -s $DOTFILES/.yabairc ~/.yabairc
 ln -s $DOTFILES/.skhdrc ~/.skhdrc
