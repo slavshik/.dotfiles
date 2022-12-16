3f27bd1ced84b575c57103413da7385ff341e09b
Aliaksandr Slaushchyk <slavsik@Aliaksandrs-MacBook-Pro.local>
2022-12-16 22:17:10 +0100

    fix (install.sh): symlink path

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 DOTFILES=~/.dotfiles
 ln -s $DOTFILES/zsh/zshrc ~/.zshrc
-ln -s $DOTFILES/nvim/config/nvim ~/.config
+ln -s $DOTFILES/nvim/config/nvim ~/.config/nvim
 ln -s $DOTFILES/tmux/tmux.conf ~/.tmux.conf
 ln -s $DOTFILES/alacritty ~/.config
 ln -s $DOTFILES/.yabairc ~/.yabairc
