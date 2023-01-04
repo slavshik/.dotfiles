1493682bc65c7b1dca96a486f369c881796796f5
Alexander Slavschik <slavshik@me.com>
2023-01-04 11:00:23 +0100

    fix (Telescope): shortcuts CMD+P, CMD+F12

--- nvim/after/plugin/lspsaga.lua
+++ nvim/after/plugin/lspsaga.lua
@@ -1,2 +1,1 @@
--- vim.keymap.set("n", ">", "<cmd>Lspsaga code_action<cr>", opts)
--- vim.keymap.set("n", "gr", "<cmd>Trouble lsp_references<cr>", opts)
+vim.keymap.set("n", ">", "<cmd>Lspsaga code_action<cr>")
