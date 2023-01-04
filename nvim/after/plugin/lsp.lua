c02c24055427d0a6179301b1d34f8a510b3a3992
Alexander Slavschik <slavshik@me.com>
2023-01-04 11:00:23 +0100

    fix (Telescope): shortcuts CMD+P, CMD+F12

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -26,6 +26,8 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
 	["<Down>"] = cmp.mapping.select_next_item(cmp_select),
 	["<Enter>"] = cmp.mapping.confirm({ select = true }),
 })
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 -- Copilot
 cmp_mappings["<Tab>"] = nil
 cmp_mappings["<S-Tab>"] = nil
@@ -34,7 +36,7 @@ lsp.on_attach(function(_, bufnr)
 	local opts = { buffer = bufnr, remap = false }
 	vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-	vim.keymap.set("n", "<Leader>D", vim.lsp.buf.type_definition, opts)
+	-- vim.keymap.set("n", "<Leader>D", vim.lsp.buf.type_definition, opts)
 	vim.keymap.set("n", "<Leader>gi", vim.lsp.buf.implementation, opts)
 end)
 lsp.setup()
