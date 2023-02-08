e0e0158a7a330a12f260ff4f533c21ce27c429af
Alexander Slavschik <slavshik@me.com>
2023-02-08 15:13:41 +0100

    fix (tmux): status line and cmd + num shortcuts\

--- nvim/lua/slavshik/colorscheme.lua
+++ nvim/lua/slavshik/colorscheme.lua
@@ -1,7 +1,8 @@
 local custom_gruvbox = require("lualine.themes.gruvbox")
+custom_gruvbox.insert.c.bg = "#ff0000"
 vim.o.background = "dark"
 vim.cmd([[colorscheme gruvbox]])
 vim.cmd([[hi normal guibg=None]])
 require("lualine").setup({
-	options = { theme = "gruvbox" },
+	options = { theme = custom_gruvbox },
 })
