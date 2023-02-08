aade44d054a3ef27872a0ea51e8ff9a11c73eb9c
Alexander Slavschik <slavshik@me.com>
2023-02-08 15:15:33 +0100

    fix: telescope filebrowser and spectre

---
+++ nvim/after/plugin/spectre.lua
@@ -0,0 +1,12 @@
+local spectre = require("spectre")
+-- TODO: --
+-- vim.keymap.set("n", "<leader>ss", function()
+-- 	spectre.open_file_search()
+-- end)
+-- nnoremap <leader>S <cmd>lua require('spectre').open()<CR>
+--
+-- -- "search current word
+-- nnoremap <leader>sw <cmd>lua require('spectre').open_visual({select_word=true})<CR>
+-- vnoremap <leader>s <esc>:lua require('spectre').open_visual()<CR>
+-- -- "  search in current file
+-- nnoremap <leader>sp viw:lua require('spectre').open_file_search()<cr>
--- nvim/lua/slavshik/packer.lua
+++ nvim/lua/slavshik/packer.lua
@@ -1,4 +1,15 @@
-vim.cmd.packadd("packer.nvim")
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
+end
+
+local packer_bootstrap = ensure_packer()
 
 return require("packer").startup(function(use)
 	use("wbthomason/packer.nvim")
@@ -7,12 +18,14 @@ return require("packer").startup(function(use)
 	use("nvim-lualine/lualine.nvim")
 
 	use({ "nvim-lua/telescope.nvim", tag = "0.1.0", requires = { { "nvim-lua/plenary.nvim" } } })
+	use({ "nvim-telescope/telescope-file-browser.nvim" })
 	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
 	use("nvim-treesitter/playground")
 	use("mbbill/undotree")
 	-- git
 	-- use("tpope/vim-fugitive")
 	use("kdheepak/lazygit.nvim")
+	use("ap/vim-css-color")
 	-- file browser
 	use({
 		"lmburns/lf.nvim",
@@ -48,6 +61,12 @@ return require("packer").startup(function(use)
 		end,
 	})
 	use({ "jose-elias-alvarez/null-ls.nvim" })
+	use({
+		"windwp/nvim-spectre",
+		requires = {
+			{ "nvim-lua/plenary.nvim" },
+		},
+	})
 	-- pettier
 	use({
 		"MunifTanjim/prettier.nvim",
