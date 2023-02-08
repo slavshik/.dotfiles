51e14f79ffcddd4d8861ee534b14ebfdbfc2a295
Alexander Slavschik <slavshik@me.com>
2023-02-08 21:49:30 +0100

    fix: new shortcuts

--- install.sh
+++ install.sh
@@ -4,6 +4,5 @@ ln -s $DOTFILES/nvim/ ~/.config/nvim
 ln -s $DOTFILES/tmux/tmux.conf ~/.tmux.conf
 ln -s $DOTFILES/alacritty ~/.config/alacritty
 ln -s $DOTFILES/karabiner ~/.config/karabiner
-ln -s $DOTFILES/.yabairc ~/.yabairc
-ln -s $DOTFILES/.skhdrc ~/.skhdrc
+ln -s $DOTFILES/.gitconfig ~/.gitconfig
 # git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
