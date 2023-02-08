2215891b5a8bbd9f2ed80515b6de306c08032690
Alexander Slavschik <slavshik@me.com>
2023-02-08 15:15:33 +0100

    fix: telescope filebrowser and spectre

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,4 +1,57 @@
+local telescope = require("telescope")
+local fb_actions = telescope.extensions.file_browser.actions
+
+telescope.setup({
+	defaults = {
+		layout_config = {
+			horizontal = {
+				width = 0.9,
+				height = 0.9,
+				preview_width = 0.5,
+			},
+			vertical = {
+				width = 0.9,
+				height = 0.9,
+				preview_height = 0.5,
+			},
+		},
+	},
+	pickers = {
+		find_files = {
+			theme = "ivy",
+		},
+		lsp_document_symbols = {
+			theme = "dropdown",
+		},
+	},
+	extensions = {
+		file_browser = {
+			layout_strategy = "center",
+			layout_config = {
+				prompt_position = "bottom",
+				width = 0.9,
+				height = 0.9,
+			},
+			-- disables netrw and use telescope-file-browser in its place
+			-- hijack_netrw = true,
+			mappings = {
+				["i"] = {
+					-- your custom insert mode mappings
+				},
+				["n"] = {
+					["-"] = fb_actions.goto_parent_dir,
+				},
+			},
+		},
+	},
+})
+require("telescope").load_extension("file_browser")
+vim.keymap.set("n", "<leader>O", function()
+	require("telescope").extensions.file_browser.file_browser()
+end, {})
+
 local builtin = require("telescope.builtin")
+
 vim.keymap.set("n", "<leader>T", vim.cmd.Telescope)
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 -- remove duplicate
