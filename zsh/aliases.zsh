70dee47a45a4357c5b01ee217cc9e2ffa7af7818
Alexander Slavschik <slavshik@me.com>
2023-02-10 13:25:25 +0100

    fix (zsh/lf): added convinient keybindings

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,15 @@
-#SSL_PATH=~/TODO add path
 function https() {
+    SSL_PATH=~/.config/ssl
     http-server -S -C $SSL_PATH/key.pem -K $SSL_PATH/key.key -p 8080
 }
+alias zshconfig="vim ~/.zshrc"
+alias python=python3
+alias j=z
+alias t=tmux
+alias ta=tmux attach
+alias hard="git reset --hard @"
+alias love="/Applications/love.app/Contents/MacOS/love"
+alias v=nvim
+alias lg=lazygit
+alias l="exa -la --git --group-directories-first"
+alias ll="exa -l --git --group-directories-first"
