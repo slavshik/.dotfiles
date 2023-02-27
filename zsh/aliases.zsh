77cca5b6d528b9a13eb7179ea224bb02ad25b6ed
Alexander Slavschik <slavshik@me.com>
2023-02-27 13:13:20 +0100

    fix (.zsh): added gh clone repo picker

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,6 +2,16 @@ function https() {
     SSL_PATH=~/.config/ssl
     http-server -S -C $SSL_PATH/key.pem -K $SSL_PATH/key.key -p 8080
 }
+# clone the repo using `gh list` and `fzf` filter
+function glone() {
+    if [ -n "$1" ]; then
+        ORG=$1
+    else
+        ORG=$(whoami)
+    fi
+    REPO=$(gh repo list $ORG --json=name --jq=".[] .name" | fzf)
+    [ -n "$REPO" ] && gh repo clone $ORG/$REPO
+}
 alias zshconfig="vim ~/.zshrc"
 alias python=python3
 alias j=z
