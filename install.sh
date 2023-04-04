bbbdc913f83f3c0c1a4027bfabf0074e3a61e201
Alexander Slavschik <slavshik@me.com>
2023-04-04 23:29:08 +0200

    fix (install.sh): linking

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 DOTFILES=~/.dotfiles
 function dotlink() {
-    ln -s $DOTFILES/$1 $2
+    ln -sfn $DOTFILES/$1 $2
 }
 dotlink zsh/zshrc ~/.zshrc
 dotlink nvim/ ~/.config/nvim
-dotlink tmux.conf ~/.tmux.conf
-dotlink alacritty ~/.config/alacritty
-dotlink karabiner ~/.config/karabiner
+dotlink tmux/tmux.conf ~/.tmux.conf
+dotlink alacritty/ ~/.config/alacritty
+dotlink karabiner/ ~/.config/karabiner
 dotlink .gitconfig ~/.gitconfig
 # git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ./defaults_write.sh
+# TODO: install packer and brew dependencies
