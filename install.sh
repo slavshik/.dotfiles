a4fde97daa05311eba0a84a31f23fd2b9b1e02d0
Alexander Slavschik <slavshik@me.com>
2023-05-19 21:53:41 +0200

    fix: global_gitignore added

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ dotlink tmux/tmux.conf ~/.tmux.conf
 dotlink alacritty/ ~/.config/alacritty
 dotlink karabiner/ ~/.config/karabiner
 dotlink .gitconfig ~/.gitconfig
+dotlink .gitignore_system ~/.gitignore
 dotlink .ideavimrc ~/.ideavimrc
 # git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ./defaults_write.sh
