057366adda44ead2b561a11bf2e01dd98162d065
Alexander Slavschik <slavshik@me.com>
2024-06-05 12:55:17 +0300

    fix (nvim-tree): remove in flavor of Oil

--- nvim/after/plugin/nvim-tree.lua
+++
@@ -1,8 +0,0 @@
-require("nvim-tree").setup({
-	live_filter = {
-		prefix = "[FILTER]: ",
-		always_show_folders = false, -- Turn into false from true by default
-	},
-})
-
-vim.keymap.set({ "n", "v" }, "<C-p>", ":NvimTreeFindFileToggle<CR>", { noremap = true, silent = true })
