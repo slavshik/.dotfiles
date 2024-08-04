384e59b39a80277102f6def6f3869ab773069983
Alexander Slavschik <slavshik@me.com>
2024-08-05 00:14:17 +0200

    fix: ui, prettier, eslint

--- nvim/lua/plugins/prettier.lua
+++ nvim/lua/plugins/prettier.lua
@@ -1,14 +1,20 @@
 return {
-    "MunifTanjim/prettier.nvim",
-    dependencies = {
-        { "neovim/nvim-lspconfig" },
-        { "jose-elias-alvarez/null-ls.nvim" },
-    },
-    opts = {
-        bin = "prettierd",
-    },
-    cond = function()
-        local prettier = require("prettier")
-        return prettier.config_exists({ check_package_json = true })
-    end
+	"MunifTanjim/prettier.nvim",
+	dependencies = {
+		{ "neovim/nvim-lspconfig" },
+		{ "jose-elias-alvarez/null-ls.nvim" },
+	},
+	config = function()
+		local prettier = require("prettier")
+
+		prettier.setup({
+			["null-ls"] = {
+				bin = "prettierd",
+				condition = function()
+					return prettier.config_exists({ check_package_json = true })
+				end,
+				timeout = 5000,
+			},
+		})
+	end,
 }
