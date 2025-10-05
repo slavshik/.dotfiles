d5940d2bf4e02e98b8a8767c599ffd2e083d612c
Alexander Slavschik <slavshik@me.com>
2025-10-05 14:46:06 +0200

    refactor(aicommit.sh): update command formatting and improve messaging guidance

--- aicommit.sh
+++ aicommit.sh
@@ -1,15 +1,29 @@
 aichat "Please suggest 3 commit messages, given the following diff:
 
         \`\`\`diff
-        $(git --no-pager diff --no-color --no-ext-diff --cached)     
+       `git --no-pager diff --no-color --no-ext-diff --cached`     
         \`\`\`
 
         **Criteria:**
 
-        1. **Format:** Should comply conventional commits format
-        Just commit messages list and nothing else. 
+        \`\`\`
+        <type>(optional scope): <description>
+        \`\`\`
 
-        Write your 3 commit messages below in the format shown in Output Template section above." \
+     - \`<type>\`: This defines the nature of the commit and is typically one of these:
+         - feat: A new feature
+         - fix: A bug fix
+         - docs: Documentation-only changes
+         - style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc.)
+         - refactor: A code change that neither fixes a bug nor adds a feature
+         - perf: A code change that improves performance
+         - test: Adding missing tests or correcting existing tests
+         - chore: Changes to the build process or auxiliary tools and libraries such as documentation generation
+         - build: Changes that affect the build system or external dependencies
+         - ci: Changes to CI configuration files and scripts
+
+    Just commit message and nothing else, without index number and newlines between.
+    Write your 3 commit messages below in the format shown in Output Template section above." \
           | fzf --height 40% --border --ansi --preview "echo {}" --preview-window=up:wrap \
           | xargs -S1024 -I {} bash -c '
               COMMIT_MSG_FILE=$(mktemp)
