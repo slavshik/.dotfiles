4bb9c57bc36d7decf4db2f6ed93d583f5935865d
Alexander Slavschik <slavshik@me.com>
2025-12-29 14:13:01 +0100

    feat: improve oil with splits

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,4 +1,16 @@
 return {
-    init = function() vim.keymap.set({"n", "v"}, "-", vim.cmd.Oil, {desc = "Open oil"} ) end,
+    init = function() 
+        vim.keymap.set("n", "-", function()
+            -- Get all windows in the current tab page
+            local wins = vim.api.nvim_tabpage_list_wins(0)
+
+            -- If only 1 window is open, create the split first
+            if #wins == 1 then
+                vim.cmd("vsplit | wincmd H")
+            end 
+            require("oil").open()
+        end, 
+        { desc = "Open Oil in vertical split" })
+    end,
     "stevearc/oil.nvim", opts = {default_file_explorer = true},
 }
