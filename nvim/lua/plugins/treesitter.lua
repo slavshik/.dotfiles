f26091d916b02b6ecdfc8434b633d2b292ef24f2
Alexander Slavschik <slavshik@me.com>
2026-01-05 10:43:10 +0100

    fix (lsp): blink, treesitter and new remap for copy text

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,17 +3,19 @@ return {
     build = ":TSUpdate",
     lazy = false,
     branch = "main",
-    opts = {
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-    },
-    -- config = function()
-    --     local configs = require("nvim-treesitter.configs")
-    --
-    --     configs.setup({
-    --         ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-    --         sync_install = false,
-    --         highlight = { enable = true },
-    --         indent = { enable = true },
-    --     })
-    -- end,
+    config = function()
+        local langs = {
+            'c', 'lua', 'vim', 'vimdoc', 'query', 'javascript', 'html', 'typescript', 'tsx'
+        }
+        -- Install parsers
+        require('nvim-treesitter').install(langs)
+
+        -- Enable treesitter highlighting for all supported filetypes
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = langs,
+            callback = function()
+                vim.treesitter.start()
+            end,
+        })
+    end,
 }
