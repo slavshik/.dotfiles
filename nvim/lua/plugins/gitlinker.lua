ccb1305c2c3f9a3e057ee02328ed5fb3bf534a63
Alexander Slavschik <slavshik@me.com>
2026-03-27 14:35:03 +0100

    feat(gitlinker): add gitlinker.nvim plugin configuration for GitLink command

---
+++ nvim/lua/plugins/gitlinker.lua
@@ -0,0 +1,24 @@
+return {
+	"linrongbin16/gitlinker.nvim",
+	cmd = "GitLink",
+	keys = {
+		{ "R", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Copy git permalink" },
+		{ "<leader>G", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git permalink in browser" },
+	},
+	opts = function()
+		local routers = require("gitlinker.routers")
+		return {
+			router = {
+				browse = {
+					["^gitlab%.gosystem%.io"] = routers.gitlab_browse,
+				},
+				default_branch = {
+					["^gitlab%.gosystem%.io"] = routers.gitlab_default_branch,
+				},
+				current_branch = {
+					["^gitlab%.gosystem%.io"] = routers.gitlab_current_branch,
+				},
+			},
+		}
+	end,
+}
