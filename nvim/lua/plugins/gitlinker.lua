b750e28ce278ad9d717782df7c557d376ab58206
Alexander Slavschik <slavshik@me.com>
2026-03-30 08:51:23 +0200

    fix: gitlinker for evolution

--- nvim/lua/plugins/gitlinker.lua
+++ nvim/lua/plugins/gitlinker.lua
@@ -11,12 +11,15 @@ return {
 			router = {
 				browse = {
 					["^gitlab%.gosystem%.io"] = routers.gitlab_browse,
+					["^gitlab%.evolution%.com"] = routers.gitlab_browse,
 				},
 				default_branch = {
 					["^gitlab%.gosystem%.io"] = routers.gitlab_default_branch,
+					["^gitlab%.evolution%.com"] = routers.gitlab_default_branch,
 				},
 				current_branch = {
 					["^gitlab%.gosystem%.io"] = routers.gitlab_current_branch,
+					["^gitlab%.evolution%.com"] = routers.gitlab_current_branch,
 				},
 			},
 		}
