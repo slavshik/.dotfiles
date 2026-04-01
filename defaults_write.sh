9c350ec7bbd2818cd56e8d888c2ffde5e1d6c93e
Alexander Slavschik <slavshik@me.com>
2026-04-01 14:01:20 +0200

    add TouchID in terminal notes to defaults_write.sh

--- defaults_write.sh
+++ defaults_write.sh
@@ -1,3 +1,8 @@
 defaults write -g ApplePressAndHoldEnabled -bool false
 defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+
+# to enable TouchID in terminal:
+# sudo nvim /etc/pam.d/sudo_local
+# auth       optional       /opt/homebrew/lib/pam/pam_reattach.so
+# auth       sufficient     pam_tid.so
