d2d3a97271c35ccbe62f40c05684174c39dfe17b
Alexander Slavschik <slavshik@me.com>
2026-05-29 14:08:40 +0200

    feat(nvim): override css/pcss icons to match lf and lazygit

--- nvim/lua/plugins/nvim-web-devicons.lua
+++ nvim/lua/plugins/nvim-web-devicons.lua
@@ -1,1 +1,20 @@
-return { "nvim-tree/nvim-web-devicons", opts = {} }
+return {
+    "nvim-tree/nvim-web-devicons",
+    opts = {
+        -- Match CSS/PCSS icons used in lf and lazygit.
+        override_by_extension = {
+            css = {
+                icon = "",
+                color = "#663399",
+                cterm_color = "91",
+                name = "Css",
+            },
+            pcss = {
+                icon = "",
+                color = "#663399",
+                cterm_color = "91",
+                name = "Pcss",
+            },
+        },
+    },
+}
